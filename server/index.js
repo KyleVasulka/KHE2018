a862681be1a4ba00610566431324afe561991082
rpascal-Laptop <ryanpascal99@yahoo.com>
2018-10-19 23:31:49 -0400

    remove gitignore in root

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('SHIT');
+const Server = require('socket.io');
+const PORT   = 3030;
+const server = require('http').Server();
+
+const io = Server(PORT);
+
+io.close(); // Close current server
+
+server.listen(PORT); // PORT is free to use
+
+io = Server(server);
