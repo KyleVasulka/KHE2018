14bc1265e8d4c0f8749a956f5309ea66c5e8848f
kjv13 <[kjv13@zips.uakron.edu]>
2018-10-21 01:34:54 -0400

    everything in assets

--- pARty play/Assets/TestConnection/ResolveS.cs
+++ pARty play/Assets/TestConnection/ResolveS.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class ResolveS : MonoBehaviour {
     public GameObject AnchControl;
     private CloudAnchorController CAnchorController;
+    public GameDataRelay gameDataRelay;
 
     // Use this for initialization
     void Start()
@@ -17,5 +18,18 @@ public class ResolveS : MonoBehaviour {
     {
         CAnchorController = AnchControl.GetComponent<CloudAnchorController>();
         CAnchorController._ResolveAnchorFromId(payload.CloudID);
+    }
+    private AnchorPayload ourPayload;
+    public void Update()
+    {
+        if (gameDataRelay.getUser().trigger_get_anchor)
+        {//update the AnchorPayload
+            gameDataRelay.getUser().trigger_get_anchor = false;
+            AnchorPayload ourPayload = gameDataRelay.getAnchorPayload();
+            GameObject eventSystem = GameObject.Find("EventSystem");
+            ResolveS resolveS = eventSystem.AddComponent<ResolveS>();
+            resolveS.ResolveAnchor(ourPayload);
+
+        }
     }
 }
