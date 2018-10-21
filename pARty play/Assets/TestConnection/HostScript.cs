036f246b56b24e10bef0fb93d9a1b5cc7448c570
kjv13 <[kjv13@zips.uakron.edu]>
2018-10-21 00:58:11 -0400

    google localization

---
+++ pARty play/Assets/TestConnection/HostScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HostScript : MonoBehaviour {
+    public GameObject EventSystem;
+    private GameDataRelay gameDataRelay;
+
+
+    // Use this for initialization
+    void Start () {
+        gameDataRelay = EventSystem.GetComponent<GameDataRelay>();
+
+
+    }
+
+    public void onHosting()
+    {
+
+        gameDataRelay.createRoom();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ pARty play/Assets/TestConnection/ResolveS.cs
@@ -0,0 +1,21 @@
+﻿using GoogleARCore.Examples.CloudAnchors;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolveS : MonoBehaviour {
+    public GameObject AnchControl;
+    private CloudAnchorController CAnchorController;
+
+    // Use this for initialization
+    void Start()
+    {
+        AnchControl = GameObject.Find("CloudAnchorController");
+
+    }
+    public void ResolveAnchor(AnchorPayload payload)
+    {
+        CAnchorController = AnchControl.GetComponent<CloudAnchorController>();
+        CAnchorController._ResolveAnchorFromId(payload.CloudID);
+    }
+}
